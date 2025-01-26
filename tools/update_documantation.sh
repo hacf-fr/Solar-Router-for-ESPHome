d5ce4a491d9994954c6d2dc8b31bf865e1e7b82c
Xavier Berger <berger.xavier@gmail.com>
2025-01-26 20:54:55 +0100

    chore: Update tool to generate documentation and changelog

--- tools/update_documantation.sh
+++ tools/update_documantation.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_DIR=$(dirname "$0")
 pushd "${SCRIPT_DIR}/.." > /dev/null
-git cliff > docs/changelog.md
-code docs/changelog.md
+git cliff > docs/en/changelog.md
+code docs/en/changelog.md
 
 read -r -p "Ready to publish? [y/N]: " response
 
 if [[ "$response" =~ ^[Yy]$ ]]; then
     mkdocs gh-deploy
 fi
-git restore docs/changelog.md
+git restore docs/en/changelog.md
 
 popd > /dev/null
