ee27b6abfdc627f198878873a0347ab00c29245a
Xavier Berger <xavier.berger@biologic.net>
2026-01-31 12:55:22 +0100

    ci: build examples in CI

--- tools/compile_all_remote_yaml.sh
+++ tools/compile_all_remote_yaml.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+GIT_BRANCH=$(git branch --show-current)
 for iloop in $(ls *.yaml | grep -v secrets | grep -v local); do
    echo
    echo "#########################################"
-   echo Compiling $iloop
-   esphome config $iloop || exit 1
+   echo Verifying $iloop
+   esphome -s github_branch ${GIT_BRANCH} config $iloop || exit 1
 done
 
 for iloop in $(ls *.yaml | grep -v secrets | grep -v local); do
    echo
    echo "#########################################"
    echo Compiling $iloop
-   esphome compile $iloop || exit 1
+   esphome -s github_branch ${GIT_BRANCH} compile $iloop || exit 1
 done
    
